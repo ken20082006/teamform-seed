5b5c2b043e3544abb4741d97a95225cae19d3e24
cyload <cyload@connect.ust.hk>
2016-11-23 15:59:53 +0800

    seperate js file

--- app/js/createCourses.js
+++ app/js/createCourses.js
@@ -1,89 +1,165 @@
-// inject firebase service
-//var app = angular.module("createCoursesApp", ["firebase"]); 
-
+	
 app.controller("createCoursesCtrl", function($scope,$rootScope,user, $firebaseArray) {
-
-		// sync with firebaseArray
+	
+		/*initialzation and checking*/
+		var courses = firebase.database().ref("courses");
+		$scope.courses = $firebaseArray(courses);
 		var userAccount = firebase.database().ref("UserAccount");
 		$scope.userAccount = $firebaseArray(userAccount);
 		
-		var courses = firebase.database().ref("courses");
-		$scope.courses = $firebaseArray(courses);
-	
-	/*	$scope.accountInfo=
-		{
-			name:"",
-			email:"",
-			role:""
+		$scope.doRedirect=function (href) {
+			window.location = href;
+		}
 		
-		};*/
+		$scope.redirect=function()
+		{
+			if($scope.role!="1")
+			{
+				 $scope.doRedirect("index.html");// only the teacher role can enter create course page
+			}
+			
+		}
+		
+		$scope.initDatePicker= function ()
+		{
+			$('#datepicker').datepicker({
+				format: 'dd/mm/yyyy',
+				startDate: '-0d'
+			});
+		}
+		
+		$scope.updateRole=function()
+		{
+			$scope.email=user.email;
+			$scope.role=user.role;
+			$scope.userName=user.userName;
+			$scope.key=user.key;
+		}
+	
+		$rootScope.$on("updateRole", function(){
+			   $scope.updateRole();
+				$scope.redirect();
+				$scope.initDatePicker();
+		});
+		
+
+		/*create course functions*/
+		
+		File.prototype.convertToBase64 = function(callback){		//function of  convert uploaded image to base64 image
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				 callback(e.target.result)
+			};
+			reader.onerror = function(e) {
+				 callback(null);
+			};        
+			reader.readAsDataURL(this);
+		};
+		
+		$scope.fileNameChanged = function (ele) // trigger when user upload a file
+		{
+		  var file = ele.files[0];
+		  if(file.type.length>0&&file.type.substr(0,5)=="image")  // check for the format type of the chose file
+		  {
+				file.convertToBase64(function(base64)
+				{
+					$scope.courseInfo.image=base64;
+					$scope.fileName=file.name;
+					$('#base64PicURL').attr('src',base64);
+					$('#base64Name').html(file.name);
+					$('#removeURL').show();
+					$('#profilePic').val('');
+				}); 		  	  
+		  }
+		  else
+		  {
+			  alert("invliad file format");
+			  $scope.removeImg();
+			  $('#profilePic').val('');
+		  }
+
+		}
 		
 		$scope.courseInfo=
 		{
 			title:"",
-			image:"",
+			image:"image/grey.png",
 			owner:"",
-			message:""
+			message:"",
+			max:"",
+			min:"",
+			date:"",
+			random:false
+		}
+		$scope.fileName;
+		
+		$scope.removeImg=function(){ //function for remove the uploaded image
+		
+			$('#removeURL').hide();
+			$('#base64Name').html('');
+			$scope.courseInfo.image='image/grey.png';
+			$scope.fileName='';
+			$('#base64PicURL').attr('src','');
+
 		}
 		
-		/*function getUserInfo(email)
-		{	
-			userAccount.orderByChild("email").equalTo(email).on("child_added", function(data)
+		$scope.validInput=function ()// check if any empty input of essential data or invalid input
+		{
+			if($scope.courseInfo.title==""||$scope.courseInfo.message==""||$scope.courseInfo.min==""||$scope.courseInfo.max==""||$scope.courseInfo.date=="")
 			{
-				
-				$scope.accountInfo.role=data.val().role;
-				$scope.accountInfo.email=data.val().email;
-				console.log($scope.accountInfo);
-				if($scope.accountInfo.role=="0")
-				{
-					alert("you are logined as studnet");
-					 window.location = "index.html";
-				}
-				else
-				{
-					alert("you are logined as teacher");
-					
-				}
-			});
-
-		}*/
-		alert(12);
-	
-		/*function isLogined()
-		{
-				firebase.auth().onAuthStateChanged(function(user) {
-				  if (user) 
-				  {
-					// User is signed in.
-					//alert("login successed");
-					getUserInfo(user.email);
-
-				  }
-				  else
-				  {
-					//  alert("not logined");
-					  window.location = "../login.html";
-				  }
-				});
+				alert("some missing data");
+				return false;
+			}
+			if($scope.courseInfo.min>=$scope.courseInfo.max)
+			{
+				alert("min should be small than max");
+				return false;
+			}
+			return true;	
 		}
 		
-		var init=function()
-		{
-			isLogined();
-		};
-		init();*/
-		
+		/*create course flow*/
+		/*
+			add an entry in courses table
+			add the course id to the course array in the  course's owner user
+		*/
 		
 		$scope.createCourse = function() {
-	
-			$scope.courseInfo.owner=$scope.accountInfo.email;
-			console.log($scope.accountInfo.email);
-			console.log($scope.courseInfo);
-			$scope.courses.$add($scope.courseInfo);
-		
+
+			var isError=false;
+			if($scope.validInput())
+			{
+				$scope.courseInfo.owner=$scope.email;
+			
+				$scope.courses.$add($scope.courseInfo).then(function(){
+					var courseArray=[];
+					courses.orderByChild("owner").equalTo($scope.email).on("child_added", function(data)
+					{
+						courseArray.push(data.getKey());
+
+						firebase.database().ref("UserAccount/"+$scope.key).once('value', function(data) {
+				
+							var newUserData=data.val();
+							newUserData.course=courseArray;
+							firebase.database().ref("UserAccount/"+$scope.key).set(newUserData);
+						});						
+						isError=false;
+					});
+
+				}, function(error) {
+				  isError=true;
+				  alert("some error occur");
+				  console.error(error);
+				}).then(function(){
+					
+					if(!isError)
+					{
+						 window.location = "index.html";
+					}			
+				});		
+				
+			}
+
 		}
-		
 
 	});
-
-
